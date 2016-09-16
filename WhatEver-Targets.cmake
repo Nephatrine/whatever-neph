fbe3aa533d81cd37dda38432acf0ab09416e683a
Daniel Wolf <nephatrine@pos.nxp>
2016-09-16 06:24:43 -0400

    added function: we_target_build_library

--- WhatEver-Targets.cmake
+++ WhatEver-Targets.cmake
@@ -1,3 +1,4 @@
+include(GenerateExportHeader)
 include(GNUInstallDirs)
 
 # ------------------------------
@@ -219,5 +220,131 @@ function(we_target_add_paths)
 				PATTERN "internal" EXCLUDE)
 		endforeach()
 	endif()
+endfunction()
+
+# ------------------------------
+# we_target_build_library
+# builds a library target
+#
+
+function(we_target_build_library VAR library soversion)
+	set(FUNC_MODE SOURCE)
+	foreach(arg ${ARGN})
+
+		# control words
+		if("${arg}" STREQUAL "SOURCE")
+			set(FUNC_MODE SOURCE)
+		elseif("${arg}" STREQUAL "EXPORT")
+			set(FUNC_MODE EXPORT)
+		elseif("${arg}" STREQUAL "TAG")
+			set(FUNC_MODE TAG)
+
+		# get lists
+		elseif("${FUNC_MODE}" STREQUAL "SOURCE")
+			list(APPEND TARG_SOURCE ${arg})
+		elseif("${FUNC_MODE}" STREQUAL "EXPORT")
+			list(APPEND TARG_INCLUDE ${arg})
+			if(NOT TARG_EXPORT)
+				set(TARG_EXPORT ${arg})
+			endif()
+		elseif("${FUNC_MODE}" STREQUAL "TAG")
+			set(TARG_TAG ${arg})
+		endif()
+	endforeach()
+
+	# what library types to build
+	string(TOUPPER "BUILD_${library}_SHARED" HAS_SHARED)
+	string(TOUPPER "BUILD_${library}_STATIC" HAS_STATIC)
+	if(BUILD_SHARED_LIBS AND NOT DEFINED ${HAS_SHARED})
+		set(${HAS_SHARED} ON)
+	endif()
+	if(NOT ${HAS_SHARED} AND NOT DEFINED ${HAS_STATIC})
+		set( ${HAS_STATIC} ON)
+	endif()
+
+	# target names
+	if(${HAS_SHARED})
+		set(TARG_SHARED "${library}")
+		set(TARG_STATIC "${library}_static")
+	else()
+		set(TARG_STATIC "${library}")
+	endif()
+
+	# configure static library
+	if(${HAS_STATIC})
+		add_library(${TARG_STATIC} STATIC ${TARG_SOURCE})
+
+		# avoid conflicts between static lib and shared import lib names
+		if(MSVC)
+			set_target_properties(${TARG_STATIC} PROPERTIES
+				OUTPUT_NAME "${library}-${soversion}${TARG_TAG}-static"
+				INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+		else()
+			set_target_properties(${TARG_STATIC} PROPERTIES
+				OUTPUT_NAME "${library}-${soversion}${TARG_TAG}"
+				INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+		endif()
+
+		# don't export symbols
+		string(TOUPPER "${library}_STATIC_DEFINE" TARG_DEF_STATIC)
+		target_compile_definitions(${TARG_STATIC} PUBLIC ${TARG_DEF_STATIC})
+
+		# install files
+		install(TARGETS ${TARG_STATIC}
+			EXPORT ${PROJECT_NAME}
+			COMPONENT static
+			LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+			ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
+		list(APPEND TARG_ALL ${TARG_STATIC})
+		set("${VAR}_STATIC" ${${VAR}_STATIC} ${TARG_STATIC}
+			PARENT_SCOPE)
+	endif()
+
+	# confgure shared library
+	if(${HAS_SHARED})
+		add_library(${TARG_SHARED} SHARED ${TARG_SOURCE})
+
+		# some platforms don't do soversions
+		if(WIN32)
+			set_target_properties(${TARG_SHARED} PROPERTIES
+				OUTPUT_NAME "${library}-${soversion}${TARG_TAG}"
+				VERSION ${PROJECT_VERSION}
+				INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+		else()
+			set_target_properties(${TARG_SHARED} PROPERTIES
+				OUTPUT_NAME "${library}${TARG_TAG}"
+				SOVERSION ${soversion}
+				VERSION ${PROJECT_VERSION}
+				INTERPROCEDURAL_OPTIMIZATION_RELEASE ON)
+		endif()
+
+		# export symbols
+		target_compile_definitions(${TARG_SHARED} PRIVATE "${library}_EXPORTS")
+
+		# install files
+		install(TARGETS ${TARG_SHARED}
+			EXPORT ${PROJECT_NAME}
+			COMPONENT shared
+			RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+			LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+			ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
+		list(APPEND TARG_ALL ${TARG_SHARED})
+		set("${VAR}_SHARED" ${${VAR}_SHARED} ${TARG_SHARED}
+			PARENT_SCOPE)
+	endif()
+
+	# generate export header
+	if(TARG_EXPORT)
+		string(REGEX REPLACE "^${CMAKE_CURRENT_BINARY_DIR}" "" TARG_EXPORT ${TARG_EXPORT})
+		set(TARG_EXPORT "${CMAKE_CURRENT_BINARY_DIR}/${TARG_EXPORT}")
+		generate_export_header(${library} BASE_NAME ${library}
+			EXPORT_FILE_NAME "${TARG_EXPORT}/${library}_api.h")
+		we_target_add_paths(${TARG_ALL} PUBLIC ${TARG_INCLUDE} INSTALL_TAG ${TARG_TAG})
+	endif()
+
+	set(${VAR} ${${VAR}} ${TARG_ALL}
+		PARENT_SCOPE)
 endfunction()
 
